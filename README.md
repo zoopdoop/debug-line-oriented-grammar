0b4fd0c17844530f740ce64795eec59d93115aea
Doug Martin <doug@zoopdoop.com>
2021-05-11 10:17:41 -0400

    Added fix for eof matching

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ To test:
 
 1. `npm install`
 2. `npm test`
+
+**UPDATE**: this issue has been debugged thanks to Marijn's quick response on the [Lezer Discuss](https://discuss.codemirror.net/t/how-to-match-end-of-file-in-line-oriented-grammars/3186).  The fixed grammer is in `fixed-example.grammar`.
--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 import { parser as newlineParser } from "./newline-example.js"
 import { parser as newlineOrEofParser } from "./newline-or-eof-example.js"
+import { parser as fixedParser } from "./fixed-example.js"
+
+console.log("\n*** DEBUG PARSER EXAMPLES:")
 
 console.log("works", newlineParser.parse("foo baz\n").toString())
 console.log("works", newlineOrEofParser.parse("foo baz\n").toString())
 
 console.log("fails", newlineParser.parse("foo baz").toString())
 console.log("fails", newlineOrEofParser.parse("foo baz").toString())
+
+console.log("\n*** FIXED PARSER EXAMPLES:")
+console.log("works", fixedParser.parse("").toString())
+console.log("works", fixedParser.parse("\n").toString())
+console.log("works", fixedParser.parse("\n\n").toString())
+console.log("works", fixedParser.parse("foo baz").toString())
+console.log("works", fixedParser.parse("foo baz\n").toString())
+console.log("works", fixedParser.parse("foo baz\nfoo bar").toString())
+console.log("works", fixedParser.parse("foo baz\nfoo bar\n\n").toString())
